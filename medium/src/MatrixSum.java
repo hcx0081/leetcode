af418015823a2d3e9ab52dda83a0d584ab1689c2
hcx0081 <623363564@qq.com>
2023-07-04 11:25:05 +0800

    -

--- medium/src/MatrixSum.java
+++ medium/src/MatrixSum.java
@@ -8,8 +8,10 @@ public class MatrixSum {
         Arrays.stream(nums).forEach(Arrays::sort);
         System.out.println(Arrays.deepToString(nums));
         int sum = 0;
+        // 列
         for (int i = 0; i < nums[0].length; i++) {
             int cur = 0;
+            // 行
             for (int j = 0; j < nums.length; j++) {
                 cur = Math.max(cur, nums[j][i]);
             }
